8f02d45736f37ffad233675f1e61cc5a656d90b1
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-25 20:21:17 +0530

    cleaned code by moving routes file content to controller

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["user", "admin"],
-      default: "admin",
+      default: "user",
     },
 
     contactNumber: String,
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,41 +1,11 @@
 const express = require("express");
+const { signup } = require("../controller/user");
 const router = express.Router();
-const User = require("../models/user");
 
 router.post("/signin", (req, res) => {
   res.send("sign in");
 });
 
-router.post("/signup", (req, res) => {
-  User.findOne({ email: req.body.email }).exec((error, user) => {
-    if (user) {
-      return res.status(400).json({
-        message: "User already exists!",
-      });
-    } else {
-      const { firstName, lastName, email, password } = req.body;
-      const _user = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-        username: Math.random().toString(),
-      });
-
-      _user.save((err, data) => {
-        if (err) {
-          return res.status(400).json({
-            message: "Something went wrong!",
-          });
-        }
-        if (data) {
-          return res.status(201).json({
-            user: data,
-          });
-        }
-      });
-    }
-  });
-});
+router.post("/signup", signup);
 
 module.exports = router;
