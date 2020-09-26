32d4b59522d9a5366c1eed01dadde5ee795f89da
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-26 14:20:09 +0530

    frontend ui build started

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <h1>Hello in body</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
